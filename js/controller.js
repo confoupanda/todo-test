a9f2ae30d3441e1092071dc3f4044c0b6bf0642b
ludovic adach <adachludovic@gmail.com>
2020-03-06 10:30:30 +0100

    premier bug trouve syntaxe sur appel de fonction addItem dans controller.js

--- js/controller.js
+++ js/controller.js
@@ -92,7 +92,7 @@
 	 * An event to fire whenever you want to add an item. Simply pass in the event
 	 * object and it'll handle the DOM insertion and saving of the new item.
 	 */
-	Controller.prototype.adddItem = function (title) {
+	Controller.prototype.addItem = function (title) { //erreur de syntaxe 3 d au lieu de 2 empechait la fonction d'etre appelé 
 		var self = this;
 
 		if (title.trim() === '') {
