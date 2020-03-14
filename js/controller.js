1ce47b9deb8b11acce36f99a908d5456a4ae595b
ludovic adach <adachludovic@gmail.com>
2020-03-14 17:46:32 +0100

    passage de controller.js en es6

--- js/controller.js
+++ js/controller.js
@@ -1,6 +1,4 @@
-(function (window) {
-	'use strict';
-
+export class Controller{
 	/**
 	 * Takes a model and view and acts as the controller between them
 	 *
@@ -8,7 +6,7 @@
 	 * @param {object} model The model instance
 	 * @param {object} view The view instance
 	 */
-	function Controller(model, view) {
+	constructor(model, view) {
 		var self = this;
 		self.model = model;
 		self.view = view;
@@ -51,48 +49,48 @@
 	 *
 	 * @param {string} '' | 'active' | 'completed'
 	 */
-	Controller.prototype.setView = function (locationHash) {
+	setView(locationHash) {
 		var route = locationHash.split('/')[1];
 		var page = route || '';
 		this._updateFilterState(page);
-	};
+	}
 
 	/**
 	 * An event to fire on load. Will get all items and display them in the
 	 * todo-list
 	 */
-	Controller.prototype.showAll = function () {
+	showAll() {
 		var self = this;
 		self.model.read(function (data) {
 			self.view.render('showEntries', data);
 		});
-	};
+	}
 
 	/**
 	 * Renders all active tasks
 	 */
-	Controller.prototype.showActive = function () {
+	showActive = function () {
 		var self = this;
 		self.model.read({ completed: false }, function (data) {
 			self.view.render('showEntries', data);
 		});
-	};
+	}
 
 	/**
 	 * Renders all completed tasks
 	 */
-	Controller.prototype.showCompleted = function () {
+	showCompleted() {
 		var self = this;
 		self.model.read({ completed: true }, function (data) {
 			self.view.render('showEntries', data);
 		});
-	};
+	}
 
 	/**
 	 * An event to fire whenever you want to add an item. Simply pass in the event
 	 * object and it'll handle the DOM insertion and saving of the new item.
 	 */
-	Controller.prototype.addItem = function (title) { //erreur de syntaxe 3 d au lieu de 2 empechait la fonction d'etre appelé 
+	addItem(title) { //erreur de syntaxe 3 d au lieu de 2 empechait la fonction d'etre appelé 
 		var self = this;
 
 		if (title.trim() === '') {
@@ -103,22 +101,22 @@
 			self.view.render('clearNewTodo');
 			self._filter(true);
 		});
-	};
+	}
 
 	/*
 	 * Triggers the item editing mode.
 	 */
-	Controller.prototype.editItem = function (id) {
+	editItem(id) {
 		var self = this;
 		self.model.read(id, function (data) {
 			self.view.render('editItem', {id: id, title: data[0].title});
 		});
-	};
+	}
 
 	/*
 	 * Finishes the item editing mode successfully.
 	 */
-	Controller.prototype.editItemSave = function (id, title) {
+	editItemSave(id, title) {
 		var self = this;
 
 		title = title.trim();
@@ -130,17 +128,17 @@
 		} else {
 			self.removeItem(id);
 		}
-	};
+	}
 
 	/*
 	 * Cancels the item editing mode.
 	 */
-	Controller.prototype.editItemCancel = function (id) {
+	editItemCancel(id) {
 		var self = this;
 		self.model.read(id, function (data) {
 			self.view.render('editItemDone', {id: id, title: data[0].title});
 		});
-	};
+	}
 
 	/**
 	 * By giving it an ID it'll find the DOM element matching that ID,
@@ -149,7 +147,7 @@
 	 * @param {number} id The ID of the item to remove from the DOM and
 	 * storage
 	 */
-	Controller.prototype.removeItem = function (id) {
+	removeItem(id) {
 		var self = this;
 		var items;
 		self.model.read(function(data) {
@@ -161,12 +159,12 @@
 		});
 
 		self._filter();
-	};
+	}
 
 	/**
 	 * Will remove all completed items from the DOM and storage.
 	 */
-	Controller.prototype.removeCompletedItems = function () {
+	removeCompletedItems() {
 		var self = this;
 		self.model.read({ completed: true }, function (data) {
 			data.forEach(function (item) {
@@ -175,7 +173,7 @@
 		});
 
 		self._filter();
-	};
+	}
 
 	/**
 	 * Give it an ID of a model and a checkbox and it will update the item
@@ -186,7 +184,7 @@
 	 *                          or not
 	 * @param {boolean|undefined} silent Prevent re-filtering the todo items
 	 */
-	Controller.prototype.toggleComplete = function (id, completed, silent) {
+	toggleComplete(id, completed, silent) {
 		var self = this;
 		self.model.update(id, { completed: completed }, function () {
 			self.view.render('elementComplete', {
@@ -198,13 +196,13 @@
 		if (!silent) {
 			self._filter();
 		}
-	};
+	}
 
 	/**
 	 * Will toggle ALL checkboxes' on/off state and completeness of models.
 	 * Just pass in the event object.
 	 */
-	Controller.prototype.toggleAll = function (completed) {
+	toggleAll(completed) {
 		var self = this;
 		self.model.read({ completed: !completed }, function (data) {
 			data.forEach(function (item) {
@@ -213,13 +211,13 @@
 		});
 
 		self._filter();
-	};
+	}
 
 	/**
 	 * Updates the pieces of the page which change depending on the remaining
 	 * number of todos.
 	 */
-	Controller.prototype._updateCount = function () {
+	_updateCount() {
 		var self = this;
 		self.model.getCount(function (todos) {
 			self.view.render('updateElementCount', todos.active);
@@ -231,13 +229,13 @@
 			self.view.render('toggleAll', {checked: todos.completed === todos.total});
 			self.view.render('contentBlockVisibility', {visible: todos.total > 0});
 		});
-	};
+	}
 
 	/**
 	 * Re-filters the todo items, based on the active route.
 	 * @param {boolean|undefined} force  forces a re-painting of todo items.
 	 */
-	Controller.prototype._filter = function (force) {
+	_filter(force) {
 		var activeRoute = this._activeRoute.charAt(0).toUpperCase() + this._activeRoute.substr(1);
 
 		// Update the elements on the page, which change with each completed todo
@@ -251,12 +249,12 @@
 		}
 
 		this._lastActiveRoute = activeRoute;
-	};
+	}
 
 	/**
 	 * Simply updates the filter nav's selected states
 	 */
-	Controller.prototype._updateFilterState = function (currentPage) {
+	_updateFilterState(currentPage) {
 		// Store a reference to the active route, allowing us to re-filter todo
 		// items as they are marked complete or incomplete.
 		this._activeRoute = currentPage;
@@ -268,9 +266,5 @@
 		this._filter();
 
 		this.view.render('setFilter', currentPage);
-	};
-
-	// Export to window
-	window.app = window.app || {};
-	window.app.Controller = Controller;
-})(window);
+	}
+}
